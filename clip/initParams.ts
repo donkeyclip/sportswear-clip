d6e7d8c05bbdd3c7ec0b6064869716e5a49ff5ef
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2023-03-27 10:10:37 +0300

    change initParams

--- clip/initParams.ts
+++ clip/initParams.ts
@@ -13,29 +13,29 @@ export default [
         {
           image:
             "assets/product-1.jpg",
-          title: "Bicycle",
+          title: "Forward Footwear",
           description:
-            "A bicycle for use in the city as well as on passable dirt roads. Number of gears: 21/Wheel size: 28 inches",
-          variants: ["235, 100, 52","52, 235, 174", "52, 131, 235"],
-          price: "599€",
+            "Take a Step in the Right Direction. Walk in style with our latest collection of fashionable shoes, designed to keep you ahead of the fashion curve.",
+          variants: ["127, 190, 170","204, 125, 53", "233, 229, 223"],
+          price: "159€",
         },
         {
           image:
           "assets/product-2.jpg",
-          title: "Skateboard",
+          title: "Sport Footwear",
           description:
-            "A skateboard with dimensions of 78.89cm doublekick.Design on the bottom side, non-slip surface on the top.",
+            "This shoe is a classic with a runway inspired look. It has been designed by one of our most famous designers and brings back the shape that defined the era. ",
           variants: ["235, 100, 52","52, 235, 174", "52, 131, 235"],
-          price: "75€",
+          price: "95€",
         },
         {
           image:
           "assets/product-3.jpg",
-          title: "Matte Black/Neon Yellow Helmet",
-          description:
-            "This helmet is designed for comfort in all conditions. Ideal for those who bike, skateboard, skate.",
-            variants: ["235, 100, 52","52, 235, 174", "52, 131, 235"],
-          price: "56€",
+          title: "Classic Modern Footwear",
+          description: 
+            "A modern classic: Our stylish shoe that has been a bestseller for years is now back in a new version with an updated material mix and a current design language.",
+            variants: ["21,54,69","87,127,146", "228,109,7"],
+          price: "86€",
         },
       ],
     },
